e4906fbbc8a461e3e499c4904b4f0842c33270bd
metamn <bartus.csongor@gmail.com>
2012-03-05 12:57:02 +0200

    People badge Jquery is working now

--- _site/assets/jquery.init.js
+++ _site/assets/jquery.init.js
@@ -1,9 +1,10 @@
+jQuery.noConflict();
+
 jQuery(document).ready(function(){
   
-  jQuery("#people-badge-name").click(function() {
+  jQuery(".people-badge-name").click(function() {
     var id = jQuery(this).attr('rel');
     var badge = "." + id;
-    alert(badge);
     jQuery(badge).slideToggle();
   });
   
