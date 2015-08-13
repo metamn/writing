2056d137a88daff5cbc28d909967e990961f4ae5
cs <bartus.csongor@gmail.com>
2015-08-13 17:49:48 +0300

    Started to create the Archives task

---
+++ gulp/tasks/archives.js
@@ -0,0 +1,21 @@
+// Generate archives
+
+
+// Plugins
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+
+    data = require('gulp-data'),
+    fs = require('fs'),
+    runSequence = require('run-sequence'),
+    onError = require('../utils/onError');
+
+
+// Configuration
+var paths = require('./../config');
+
+
+// Generate archives
+gulp.task('archives', function() {
+  
+});
